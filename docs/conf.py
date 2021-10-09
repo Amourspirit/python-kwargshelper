4f2e92068818628a1c3e6760ee13dd58a43354c8
Paul Moss <vibrationoflife@protonmail.com>
2021-10-09 16:47:30 -0400

    rules listed

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,13 @@
 #
 import os
 import sys
+from docutils.parsers.rst import Directive
+from docutils import nodes, statemachine
+from os.path import basename
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 sys.path.insert(0, os.path.abspath('..'))
 
 from kwhelp import __version__
@@ -70,3 +77,35 @@ if html_theme == 'sphinx_rtd_theme':
 napoleon_google_docstring = True
 napoleon_include_init_with_doc = True
 
+# https://stackoverflow.com/questions/7250659/how-to-use-python-to-programmatically-generate-part-of-sphinx-documentation/18143318#18143318
+# https://stackoverflow.com/questions/27875455/displaying-dictionary-data-in-sphinx-documentation
+# custom directive
+
+
+class ExecDirective(Directive):
+    """Execute the specified python code and insert the output into the document"""
+    has_content = True
+
+    def run(self):
+        oldStdout, sys.stdout = sys.stdout, StringIO()
+
+        tab_width = self.options.get(
+            'tab-width', self.state.document.settings.tab_width)
+        source = self.state_machine.input_lines.source(
+            self.lineno - self.state_machine.input_offset - 1)
+
+        try:
+            exec('\n'.join(self.content))
+            text = sys.stdout.getvalue()
+            lines = statemachine.string2lines(
+                text, tab_width, convert_whitespace=True)
+            self.state_machine.insert_input(lines, source)
+            return []
+        except Exception:
+            return [nodes.error(None, nodes.paragraph(text="Unable to execute python code at %s:%d:" % (basename(source), self.lineno)), nodes.paragraph(text=str(sys.exc_info()[1])))]
+        finally:
+            sys.stdout = oldStdout
+
+
+def setup(app):
+    app.add_directive('exec', ExecDirective)
--- docs/source/general/rules.rst
+++ docs/source/general/rules.rst
@@ -26,8 +26,17 @@ New rules can be created by inheriting from :py:class:`~.rules.IRule` interface/
 Included Rules
 --------------
 
-.. toctree::
-    :titlesonly:
-    :glob:
+.. exec::
 
-    ../kwhelp/rules/Rule*
+    import glob
+    import os
+    r_path = os.path.join(os.path.abspath('.'), "source/kwhelp/rules")
+    rules_pattern = r_path + os.sep + "Rule*.rst"
+    rule_list = glob.glob(rules_pattern)
+    rule_list.sort()
+    i_trim = len(r_path) + 1
+    print("\n")
+    for rule in rule_list:
+        name = str(rule)[i_trim:-4]
+        print("* :py:class:`~.rules.{}`".format(name))
+        print("\n")
