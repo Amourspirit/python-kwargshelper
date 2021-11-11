1c4d188bc0fdee2722dd710abf9cdabd5033893a
Paul Moss <vibrationoflife@protonmail.com>
2021-11-11 15:51:13 -0500

    TestFormatter test updates

--- tests/test_cls_Formatter.py
+++ tests/test_cls_Formatter.py
@@ -33,5 +33,37 @@ class TestFormatter(unittest.TestCase):
             names=['first', 'second', 'third'])
         assert result == "'first', 'second', and 'third'"
 
+    def test_get_formated_types(self):
+        types = [str, float, type(self)]
+        result = Formatter.get_formated_types(types=[str, float, type(self)])
+        assert isinstance(result, str)
+        for t in types:
+            t_name = f"'{t.__name__}'"
+            assert result.index(t_name) >= 0
+        assert result.index('and') > 0
+        result = Formatter.get_formated_types(types=[str, float, type(self)], conj='or', wrapper="")
+        assert isinstance(result, str)
+        for t in types:
+            t_name = f"{t.__name__}"
+            assert result.index(t_name) >= 0
+        assert result.index('or') > 0
+
+    def test_get_missing_args_error_msg(self):
+        names_lst = ['first', 'second', 'third']
+        name = 'myfunc()'
+        result = Formatter.get_missing_args_error_msg(missing_names=names_lst, name=name)
+        assert isinstance(result, str)
+        assert result.startswith(name)
+        for n in names_lst:
+            assert result.index(n) > 0
+        names_lst = ['first']
+        result = Formatter.get_missing_args_error_msg(
+            missing_names=names_lst)
+        assert isinstance(result, str)
+        assert result.startswith("missing")
+        for n in names_lst:
+            assert result.index(n) > 0
+        
+
 if __name__ == '__main__':
     unittest.main()
